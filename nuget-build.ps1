878f946e6cc294f69e018370852db09b4daaab12
Brian Begy <bbegy@grubhub.com>
2018-05-25 16:55:13 -0500

    build scripts

--- nuget-build.ps1
+++ nuget-build.ps1
@@ -1,2 +1,5 @@
- rm .\ConfigWrapper.*.*.*.nupkg
- nuget pack .\ConfigWrapper\ConfigWrapper.nuspec
+ MsBuild.exe .\src\Retrier.sln /t:Build /p:Configuration=Release /p:TargetFramework=v4.0
+ 
+ rm .\src\Retrier\Retrier*.*.*.nupkg
+ nuget pack .\src\Retrier\Retrier.nuspec
+
